7e223cd5690c33d6619f1824a054a1676f7a1b81
Rene <khatymovrenat@gmail.com>
2024-01-30 02:36:15 +0300

    #1 Pre semaphore commit

--- main.cpp
+++ main.cpp
@@ -12,9 +12,11 @@
 #include <fcntl.h>           /* For O_* constants */
 
 #include <unistd.h>          /* ftruncate */
+#include <thread>
 
 #include <semaphore.h>
 #include <cstdlib>
+#include <functional>
 namespace fs = std::filesystem;
 
 #define STORAGE_SIZE 24
@@ -39,10 +41,10 @@ int main(int argc, char* argv[])
     const std::string_view target_path{args[2]};
     //should be unique
     std::string shared_name = std::string(source_path) + std::string(target_path);
-    shared_name.erase(std::remove(shared_name.begin(), shared_name.end(), '/'), shared_name.end());
-    shared_name.erase(std::remove(shared_name.begin(), shared_name.end(), '.'), shared_name.end());
-    shared_name = "/" + shared_name;
+    auto shared_name_hash = std::hash<std::string>()(shared_name);
+    shared_name = "/" + std::to_string(shared_name_hash);
 
+    std::cout << "Shared object name (hash): " << shared_name << std::endl;
     if (!fs::exists(source_path.data()))
     {
         std::cout << "Source file doesn't exist." << std::endl;
@@ -59,12 +61,13 @@ int main(int argc, char* argv[])
     //обращаемся к общей памяти по имени, создаем общий объект в памяти если нету и разрешаем туда писать.
     //S_IRUSR and S_IWUSR -  read and write permission for the owner of the file
     //O_EXCL - If O_EXCL and O_CREAT are set, shm_open() fails if the shared memory object exists.
-    int fd_read = shm_open(shared_name.data(),  O_RDWR | O_CREAT | O_EXCL, 0777);
+    int fd_read = shm_open(shared_name.data(),  O_RDWR | O_EXCL | O_CREAT, 0600);
 
+    std::cout << "fd_read: " << fd_read << std::endl;
     /*
      * SERVER - publisher
      */
-    if (fd_read != -1)
+    if (fd_read > -1)
     {
         std::cout << "Server side." << std::endl;
         // устанавливаем размер общей памяти на основе page-aligned (4KB)
@@ -90,39 +93,41 @@ int main(int argc, char* argv[])
         //do
         //читаем STORAGE_SIZE байт в буффер из файла
         //записываем в общую память
-        //прочесть Use 'placement' new  ??memcpy(buffer, addr, STORAGE_SIZE)??
         //!!! продумать механизм синхронизации
         // пока кол-во прочитанных байт != 0
         // The abbreviation  "rb"  includes the representation of binary mode, as denoted by  b  code.
-        //            std::FILE* read_file = std::fopen(source_path.data(), "rb");
-        Buffer buffer;
-        buffer.size = 24;
-        memcpy(buffer.data, "Hello World             ", STORAGE_SIZE);
-        do
-        {
-            memcpy(addr, &buffer.data,  STORAGE_SIZE);
-            buffer.size = 1;
-            const bool everything_done = buffer.size == 0;
+        std::FILE* read_file = std::fopen(source_path.data(), "rb");
+        Buffer* buffer = new (addr) Buffer;
+
+        do {
+            buffer->size = std::fread(&buffer->data, sizeof(char), STORAGE_SIZE, read_file);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            const bool everything_done = buffer->size == 0;
             if (everything_done)
             {
                 break;
             }
         } while (true);
 
-        //            fclose(read_file);
+        fclose(read_file);
         // Unmap the memory when done
         if (munmap(addr, STORAGE_SIZE) == -1) {
             std::cerr << "Server: Error unmapping memory " << std::endl;
         }
+
+        shm_unlink(shared_name.data());
     }
     /*
      * Client - listener
      */
     else {
         std::cout << "Client side." << std::endl;
+        //TODO: handle the case when second client tries to open to the same shared memory
         int fd_write = shm_open(shared_name.data(),  O_RDWR, S_IRUSR | S_IWUSR);
         if (fd_write != -1)
         {
+
+
             // ссылаемся на указанную общую память-на адресное пространство процесса
             // ч/з дескриптор
             void *addr = mmap(NULL, STORAGE_SIZE, PROT_READ, MAP_SHARED, fd_write, 0);
@@ -132,21 +137,22 @@ int main(int argc, char* argv[])
                 return EXIT_FAILURE;
             }
 
-            usleep(1000);
-            //                char* buf = static_cast<char *>(addr);
-            Buffer buffer;
-            memcpy(&buffer.data, addr,  STORAGE_SIZE);
-            std::cout << "Client: msg" << buffer.data << std::endl;
+            do
+            {
+                Buffer* buffer = static_cast<Buffer*>(addr);
+                std::cout << buffer->data << std::endl;
+                if (buffer->size == 0)
+                    break;
+            } while (true);
+
             // Unmap the memory when done
             if (munmap(addr, STORAGE_SIZE) == -1) {
-                std::cerr << "Server: Error unmapping memory " << std::endl;
+                std::cerr << "Client: Error unmapping memory " << std::endl;
             }
         } else {
             std::cerr << "Client: Error to open shared memory " << std::endl;
         }
     }
-
-    shm_unlink(shared_name.data());
 
     return 0;
 }
